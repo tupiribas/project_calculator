6e1da0e9e9eb7bdea5d10b57f8ea49428a86b43a
Tupi Guedes <tupiguedes@outlook.com>
2021-09-11 09:30:48 -0300

    Main view

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,9 @@
 module project_calculator {
 	requires javafx.controls;
 	
+	requires javafx.fxml;
+	
 	opens application to javafx.graphics, javafx.fxml;
+	
+	opens gui to javafx.graphics, javafx.fxml;
 }
