bf1d12abc71dcddec038f536b398e4ff8c9a4dba
Tupi Guedes <tupiguedes@outlook.com>
2021-09-12 01:10:34 -0300

    onAddNumbersToLabel and onAddOperations

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -9,19 +9,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainViewController implements Initializable {
-
+	
 	// Cálculo e Resposta
 	@FXML
 	private Label labelCalculation;
-	
+
 	@FXML
 	private Label labelResp;
 	
-	
+
 	// Botões de Valores
 	@FXML
 	private Button btValue00;
-	
+
 	@FXML
 	private Button btValue01;
 
@@ -48,19 +48,19 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	private Button btValue09;
-	
+
 	// Botões de Operações
 	@FXML
-	private Button btValueSub;
-	
-	@FXML
-	private Button btValueSum;
+	private Button btOperationSub;
 
 	@FXML
-	private Button btValueMul;
+	private Button btOperationSum;
 
 	@FXML
-	private Button btValueDiv;
+	private Button btOperationMul;
+
+	@FXML
+	private Button btOperationDiv;
 
 	@FXML
 	private Button btValueResult;
@@ -68,10 +68,68 @@ public class MainViewController implements Initializable {
 	@FXML
 	private Button btValueFloatingPoint;
 	
+	@FXML
+	public void onAddNumbersToLabel() {
+		if (btValue00.isArmed()) {
+			labelCalculation.setText(labelCalculation.getText() + "0");
+		}
+		if (btValue01.isArmed()) {
+			labelCalculation.setText(labelCalculation.getText() + "1");
+		}
+		if (btValue02.isArmed()) {
+			labelCalculation.setText(labelCalculation.getText() + "2");
+		}
+		if (btValue03.isArmed()) {
+			labelCalculation.setText(labelCalculation.getText() + "3");
+		}
+		if (btValue04.isArmed()) {
+			labelCalculation.setText(labelCalculation.getText() + "4");
+		}
+		if (btValue05.isArmed()) {
+			labelCalculation.setText(labelCalculation.getText() + "5");
+		}
+		if (btValue06.isArmed()) {
+			labelCalculation.setText(labelCalculation.getText() + "6");
+		}
+		if (btValue07.isArmed()) {
+			labelCalculation.setText(labelCalculation.getText() + "7");
+		}
+		if (btValue08.isArmed()) {
+			labelCalculation.setText(labelCalculation.getText() + "8");
+		}
+		if (btValue09.isArmed()) {
+			labelCalculation.setText(labelCalculation.getText() + "9");
+		}
+	}
 	
+	public void onAddOperations() {
+		int cont = 0;
+
+		if (cont <= 0) {
+			if (btOperationSub.isArmed()) {
+				labelCalculation.setText(labelCalculation.getText() + " - ");
+			}
+		}
+		else if (cont <= 0) {
+			if (btOperationSum.isArmed()) {
+				labelCalculation.setText(labelCalculation.getText() + " + ");
+			}
+		}
+		else if (cont <= 0) {
+			if (btOperationMul.isArmed()) {
+				labelCalculation.setText(labelCalculation.getText() + " x ");
+			}
+		}
+		else if (cont <= 0)  {
+			if (btOperationDiv.isArmed()) {
+				labelCalculation.setText(labelCalculation.getText() + " ÷ ");
+			}
+		}
+	}
+	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		
 	}
 
 }
